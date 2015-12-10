9c92b0a8e5a41dd65d40db2b430eca792946b4c2
Karan Sahu <karansahu1991@gmail.com>
2015-12-10 16:16:03 -0500

    Room Connections.

--- Assets/CellularAutomata.cs
+++ Assets/CellularAutomata.cs
@@ -6,45 +6,39 @@ public class CellularAutomata : MonoBehaviour
 {
     System.Random rand;
     private MeshGenerator meshGen;
-
     private int[,] map;
     private Texture2D texRandomFill;
-    private GameObject quad;
 
-    public int mapWidth, mapHeight, iterationNumCA, floorPercent, wallThreshold, floorThreshold,seed, minWallRegionSize, minFloorRegionSize;
+    public int mapWidth, mapHeight, iterationNumCA, floorPercent, wallThreshold, floorThreshold, seed, minWallRegionSize, minFloorRegionSize;
 
-    void Start ()
-    {        
-        quad = GameObject.Find("Quad");
-        meshGen = GetComponent<MeshGenerator>();
-        rand = new System.Random(seed);
-    }
-	
-	void Update ()
+    void Start()
     {
-	    if(Input.GetKeyDown(KeyCode.Space))
+        meshGen = GetComponent<MeshGenerator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            texRandomFill = new Texture2D(mapWidth, mapHeight, TextureFormat.RGB24, false);
-            texRandomFill.name = "TextureRandomFill";
-            texRandomFill.wrapMode = TextureWrapMode.Clamp;
-            
+            //texRandomFill = new Texture2D(mapWidth, mapHeight, TextureFormat.RGB24, false);
+            //texRandomFill.name = "TextureRandomFill";
+            //texRandomFill.wrapMode = TextureWrapMode.Clamp;
             //calculate again
             int[,] randomMap = GenerateRandomMap();
             //GenerateTexture(randomMap);
-            int [,] smoothMap = GenerateSmoothMap(randomMap);
+            int[,] smoothMap = GenerateSmoothMap(randomMap);
             //GenerateTexture(smoothMap);
             map = smoothMap;
-
-            SearchMapForRegions(minWallRegionSize, minFloorRegionSize);
+            ScanMapForRegions();
             meshGen.GenerateMesh(smoothMap, 1);
-
         }
-	}
+    }
 
-    int [,] GenerateRandomMap()
+    int[,] GenerateRandomMap()
     {
-        int [,] map = new int[mapWidth, mapHeight];
         rand = new System.Random(seed);
+        int[,] map = new int[mapWidth, mapHeight];
+
         for (int i = 0; i < mapWidth; i++)
         {
             for (int j = 0; j < mapHeight; j++)
@@ -52,11 +46,11 @@ public class CellularAutomata : MonoBehaviour
                 //check for border and create a wall (1)
                 if (i == 0)
                     map[i, j] = 1;
-                else if(j == 0)
+                else if (j == 0)
                     map[i, j] = 1;
-                else if(i == mapWidth - 1)
+                else if (i == mapWidth - 1)
                     map[i, j] = 1;
-                else if(j == mapHeight - 1)
+                else if (j == mapHeight - 1)
                     map[i, j] = 1;
                 //check if middle and create a wall (1) and create a floor or wall depending on a percentage
                 else
@@ -66,7 +60,7 @@ public class CellularAutomata : MonoBehaviour
         return map;
     }
 
-    int[,] GenerateSmoothMap(int [,] map)
+    int[,] GenerateSmoothMap(int[,] map)
     {
         for (int iteration = 0; iteration < iterationNumCA; iteration++)
         {
@@ -94,7 +88,7 @@ public class CellularAutomata : MonoBehaviour
                     }
                     //else I am floor
                     else
-                        map[i,j] = 0;
+                        map[i, j] = 0;
                 }
             }
         }
@@ -113,11 +107,11 @@ public class CellularAutomata : MonoBehaviour
                 if (!(x == posX && y == posY))
                 {
                     //Out of bounds
-                    if (!IsInsideMap(x,y))
+                    if (!IsInsideMap(x, y))
                     {
                         wallCount++;
                         continue;
-                    }                    
+                    }
                     //If wall
                     if (map[x, y] == 1)
                         wallCount++;
@@ -125,11 +119,6 @@ public class CellularAutomata : MonoBehaviour
             }
         }
         return wallCount;
-    }
-
-    bool IsInsideMap(int posX, int posY)
-    {
-        return (posX >= 0 && posY >= 0 && posX < mapWidth && posY < mapHeight);
     }
 
     void GenerateTexture(int[,] values)
@@ -147,15 +136,14 @@ public class CellularAutomata : MonoBehaviour
             }
         }
         texRandomFill.Apply(false);
-        quad.GetComponent<Renderer>().material.SetTexture("_MainTex", texRandomFill);
     }
-    
-    void SearchMapForRegions(int minWallRegion, int minFloorRegion)
+
+    void ScanMapForRegions()
     {
         List<List<Coord>> wallRegions = GetAllRegions(1);
         foreach (List<Coord> region in wallRegions)
         {
-            if(region.Count < minWallRegion)
+            if (region.Count < minWallRegionSize)
             {
                 foreach (Coord c in region)
                 {
@@ -165,31 +153,93 @@ public class CellularAutomata : MonoBehaviour
         }
 
         List<List<Coord>> floorRegions = GetAllRegions(0);
+        List<Room> roomsLeftAfterRegionCrop = new List<Room>();
         foreach (List<Coord> region in floorRegions)
         {
-            if (region.Count < minFloorRegion)
+            if (region.Count < minFloorRegionSize)
             {
                 foreach (Coord c in region)
                 {
                     map[c.posX, c.posY] = 1;
                 }
             }
+            else
+                roomsLeftAfterRegionCrop.Add(new Room(region,map));
         }
+
+        FindConnectionsBetweenRooms(roomsLeftAfterRegionCrop);
+    }
+
+    void FindConnectionsBetweenRooms(List<Room> roomsToConnect)
+    {
+        int shortestDistance = 0;
+        bool connectionFound = false;
+        Room roomA_ConnectFrom = new Room();
+        Room roomB_ConnectTo = new Room();
+        Coord tileA_ConnectFrom = new Coord();
+        Coord tileB_ConnectTo = new Coord();
+
+        foreach (Room roomA in roomsToConnect)
+        {
+            foreach (Room roomB in roomsToConnect)
+            {
+                if (roomA == roomB)
+                    continue;
+
+                if (roomA.IsConnectedTo(roomB))
+                {
+                    connectionFound = false;
+                    break;
+                }
+
+                for (int i = 0; i < roomA.edgeTiles.Count; i++)
+                {
+                    for (int j = 0; j < roomB.edgeTiles.Count; j++)
+                    {
+                        Coord tileRoomA = roomA.edgeTiles[i];
+                        Coord tileRoomB = roomB.edgeTiles[i];
+
+                        int distance = (int)(Mathf.Pow((tileRoomA.posX - tileRoomB.posX),2) + Mathf.Pow((tileRoomA.posY - tileRoomB.posY),2));
+                        if(distance < shortestDistance || !connectionFound)
+                        {
+                            connectionFound = true;
+                            roomA_ConnectFrom = roomA;
+                            roomB_ConnectTo = roomB;
+                            tileA_ConnectFrom = tileRoomA;
+                            tileB_ConnectTo = tileRoomB;
+                        }
+                    }
+                }
+                if (connectionFound)
+                    CreatePassage(roomA_ConnectFrom,roomB_ConnectTo,tileA_ConnectFrom,tileB_ConnectTo);
+            }
+        }
+    }
+
+    void CreatePassage(Room roomA, Room roomB, Coord tileA, Coord tileB)
+    {
+        Room.ConnectRooms(roomA, roomB);
+        Gizmos.DrawLine(CoordToWorld(tileA), CoordToWorld(tileB));
+    }
+
+    Vector3 CoordToWorld(Coord A)
+    {
+        return (new Vector3(-mapWidth/2 + 0.5f + A.posX, 5, -mapHeight/2 + 0.5f + A.posY));
     }
 
     List<List<Coord>> GetAllRegions(int tileType)
     {
-        List<List<Coord>> allRegions = new List<List<Coord>>();
-        int[,] tileVisited = new int[mapWidth,mapHeight];
+        List<List<Coord>> allRegionsOfTileType = new List<List<Coord>>();
+        int[,] tileVisited = new int[mapWidth, mapHeight];
 
-        for (int i = 0; i < mapWidth; i++)
+        for (int x = 0; x < mapWidth; x++)
         {
-            for (int j = 0; j < mapHeight; j++)
+            for (int y = 0; y < mapHeight; y++)
             {
-                if(map[i,j] == tileType && tileVisited[i,j] == 0)
+                if (map[x, y] == tileType && tileVisited[x, y] == 0)
                 {
-                    List<Coord> regionToAdd = GetSpecifiedRegion(i, j);
-                    allRegions.Add(regionToAdd);
+                    List<Coord> regionToAdd = GetSpecifiedRegion(x, y);
+                    allRegionsOfTileType.Add(regionToAdd);
 
                     foreach (Coord c in regionToAdd)
                     {
@@ -199,44 +249,48 @@ public class CellularAutomata : MonoBehaviour
             }
         }
 
-        return allRegions;
+        return allRegionsOfTileType;
     }
 
-    List<Coord> GetSpecifiedRegion(int tilePosX, int tilePosY)
+    List<Coord> GetSpecifiedRegion(int posX, int posY)
     {
         List<Coord> region = new List<Coord>();
-        int[,] tileVisited = new int[mapWidth, mapHeight];
-        int tileType = map[tilePosX,tilePosY];
+        int[,] tileVisisted = new int[mapWidth, mapHeight];
+        int tileType = map[posX, posY];
 
-        Queue<Coord> tileQueue = new Queue<Coord>();
-        tileQueue.Enqueue(new Coord(tilePosX, tilePosY));
-        tileVisited[tilePosX, tilePosY] = 1;
+        Queue<Coord> regionTiles = new Queue<Coord>();
+        regionTiles.Enqueue(new Coord(posX, posY));
+        tileVisisted[posX, posY] = 1;
 
-        while(tileQueue.Count > 0)
+        while (regionTiles.Count > 0)
         {
-            Coord tile = tileQueue.Dequeue();
+            Coord tile = regionTiles.Dequeue();
             region.Add(tile);
 
-            for (int i = tile.posX - 1; i <= tile.posX + 1; i++)
+            for (int x = tile.posX - 1; x <= tile.posX + 1; x++)
             {
-                for (int j = tile.posY - 1; j <= tile.posY + 1; j++)
+                for (int y = tile.posY - 1; y <= tile.posY + 1; y++)
                 {
-                    if(IsInsideMap(i, j) && (i == tile.posX || j == tile.posY))
+                    if (IsInsideMap(x, y) && (x == tile.posX || y == tile.posY))
                     {
-                        if(tileVisited[i,j] == 0 && map[i,j] == tileType)
+                        if (map[x, y] == tileType && tileVisisted[x, y] == 0)
                         {
-                            tileVisited[i, j] = 1;
-                            tileQueue.Enqueue(new Coord(i,j));
+                            tileVisisted[x, y] = 1;
+                            regionTiles.Enqueue(new Coord(x, y));
                         }
                     }
                 }
             }
         }
-
         return region;
     }
 
-    struct Coord
+    bool IsInsideMap(int posX, int posY)
+    {
+        return (posX >= 0 && posX < mapWidth && posY >= 0 && posY < mapHeight);
+    }
+
+    public struct Coord
     {
         public int posX, posY;
         public Coord(int x, int y)
@@ -246,4 +300,44 @@ public class CellularAutomata : MonoBehaviour
         }
     }
 
+    public class Room
+    {
+        public List<Coord> tiles;
+        public List<Coord> edgeTiles;
+        public List<Room> connectedRooms;
+
+        public Room() { }
+
+        public Room(List<Coord> _tiles, int[,] map)
+        {
+            tiles = _tiles;
+            edgeTiles = new List<Coord>();
+            connectedRooms = new List<Room>();
+
+            foreach (Coord c in tiles)
+            {
+                for (int x = c.posX - 1; x <= c.posX + 1; x++)
+                {
+                    for (int y = c.posY- 1; y <= c.posY + 1; y++)
+                    {
+                        if(map[x,y] == 1 && (x == c.posX || y == c.posY))
+                        {
+                            edgeTiles.Add(c);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void ConnectRooms(Room firstRoom, Room secondRoom)
+        {
+            firstRoom.connectedRooms.Add(secondRoom);
+            secondRoom.connectedRooms.Add(firstRoom);
+        }
+
+        public bool IsConnectedTo(Room room)
+        {
+            return (connectedRooms.Contains(room));
+        }
+    }
 }
